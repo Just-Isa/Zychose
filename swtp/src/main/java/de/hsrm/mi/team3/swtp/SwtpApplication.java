9fb1f460b1f4df3eddbfa59c34c9a5cb7c22b68a
Nico Ismaili <nico@ismaili.de>
2022-11-09 13:43:49 +0100

    🎉 [INIT] initialized project

---
+++ swtp/src/main/java/de/hsrm/mi/team3/swtp/SwtpApplication.java
@@ -0,0 +1,13 @@
+package de.hsrm.mi.team3.swtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwtpApplication.class, args);
+	}
+
+}
