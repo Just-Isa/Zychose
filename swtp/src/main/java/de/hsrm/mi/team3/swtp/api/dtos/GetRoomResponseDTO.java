c88cf11cb7c82e8b6a448c6cd8e83efebdce02b9
Mike <mx.daudrich@gmail.com>
2022-11-13 23:34:13 +0100

    [#39][ADDITION]:DTO Inhalt hinzugefügt

--- swtp/src/main/java/de/hsrm/mi/team3/swtp/api/dtos/GetRoomResponseDTO.java
+++ swtp/src/main/java/de/hsrm/mi/team3/swtp/api/dtos/GetRoomResponseDTO.java
@@ -1,5 +1,20 @@
 package de.hsrm.mi.team3.swtp.api.dtos;
 
-public class GetRoomResponseDTO {
-    
+import java.util.List;
+
+import de.hsrm.mi.team3.swtp.domain.Room;
+import de.hsrm.mi.team3.swtp.domain.User;
+
+public record GetRoomResponseDTO (
+    String roomName,
+    int roomNumber,
+    List<User> userList
+) {
+    public static GetRoomResponseDTO from(Room r) {
+        GetRoomResponseDTO dto = new GetRoomResponseDTO(
+            r.getRoomName(),
+            r.getRoomNumber(),
+            r.getUserList());
+        return dto;
+    }
 }
