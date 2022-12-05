c72be90463c70d7330227c65f7212d1909e7e948
Ferdinand Mentzen <ferdinand.mentzen@student.hs-rm.de>
2022-12-05 12:00:07 +0100

    [#63][REFACTOR]:Code aufgeräumt

--- frontend/src/services/JSONparser.ts
+++ frontend/src/services/JSONparser.ts
@@ -1,74 +1,47 @@
-import { TypeStreet, type IStreetInformation } from '../components/grid/useStreets';
-
-// get the readonly-street-state
-// const {streets} = useStreets();
-
-// leider doch nich so einfach
-//let list = JSON.stringify(streets);
-
-enum Rotations {
-    ZERO=0,NINETY=90,ONEEIGHTY=180,TWOSEVENTY=270
-}
+import {
+  TypeStreet,
+  type IStreetInformation,
+} from "../components/grid/useStreets";
 
 export function gridToJson(streets: IStreetInformation[]) {
+  const streettypeAsKeyMap = new Map();
 
-    let streettypeAsKeyMap = new Map();
+  const keys = Object.keys(TypeStreet).filter((v) => !isNaN(Number(v)));
+  const rotations = [0, 90, 180, 270];
 
-    const keys = Object.keys(TypeStreet).filter((v) => !isNaN(Number(v)));
+  for (const k of keys) {
+    const filteredStreets = streets.filter(
+      (element) => element.streettype === Number(k)
+    );
 
-    const rotations = [0,90,180,270];
+    console.log(
+      `FILTERED STREETS: ${filteredStreets.forEach((ele) =>
+        console.log(ele.streettype, ele.rotation, ele.posX, ele.posY)
+      )}`
+    );
 
-    // // iterate over the values (1,2,3,4) of the enum
-    // keys.forEach((stype) => {
-    //     const filteredStreets = streets.filter(element => element.streettype === Number(stype));
+    const rotationAsKeyMap = new Map();
 
-    //     console.log(`FILTERED STREETS: ${filteredStreets.forEach(ele => console.log(ele.streettype, ele.rotation, ele.posX, ele.posY))}`);
+    for (const r of rotations) {
+      const filteredRotations = filteredStreets.filter(
+        (element) => element.rotation === Number(r)
+      );
+      const positionArray = [];
 
-    //     // finds the value to the values defined it the Rotations enum
-    //     rotations.forEach((rot) => {
-    //         const filteredRotations = filteredStreets.filter(element => element.rotation === Number(rot));
-    //         let positionArray = [];
-    //         let rotationAsKeyMap = new Map();
+      for (const ele of filteredRotations) {
+        positionArray.push([ele.posX, ele.posY]);
+      }
 
-    //         for(let ele of filteredRotations) {
-    //             positionArray.push([ele.posX, ele.posY]);
-    //         }
-
-    //         rotationAsKeyMap.set(rot, positionArray);
-
-    //         streettypeAsKeyMap.set(stype, rotationAsKeyMap);
-    //     })
-
-
-    // })
-
-    // TODO: FOR-EACH war das problem, normale for schleife lösts
-
-    for (let k of keys) {
-        
-        const filteredStreets = streets.filter(element => element.streettype === Number(k));
-
-        console.log(`FILTERED STREETS: ${filteredStreets.forEach(ele => console.log(ele.streettype, ele.rotation, ele.posX, ele.posY))}`);
-
-        let rotationAsKeyMap = new Map();
-        
-        for (let r of rotations) {
-            const filteredRotations = filteredStreets.filter(element => element.rotation === Number(r));
-            let positionArray = [];
-
-            for(let ele of filteredRotations) {
-                positionArray.push([ele.posX, ele.posY]);
-            }
-
-            rotationAsKeyMap.set(r, positionArray);
-
-        }
-        streettypeAsKeyMap.set(k, rotationAsKeyMap);
-
+      rotationAsKeyMap.set(r, positionArray);
     }
+    streettypeAsKeyMap.set(k, rotationAsKeyMap);
+  }
 
-    console.log(streettypeAsKeyMap);
-    let dings = JSON.stringify(Array.from(streettypeAsKeyMap.entries()));
+  console.log(streettypeAsKeyMap);
+  const dings = JSON.stringify(
+    Array.from(Array.from(streettypeAsKeyMap.entries()).entries())
+  );
+  const dings2 = JSON.stringify(streettypeAsKeyMap.keys);
+  console.log(dings);
+  console.log(dings2);
 }
-
-
