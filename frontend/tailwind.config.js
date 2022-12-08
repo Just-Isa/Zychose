f489ab28b3310ec6564649e3c399359aef9ad0b4
Antonia Eckert <antonia.eckert@student.hs-rm.de>
2022-12-08 16:12:57 +0100

    [#50][REFACTOR]: Backfolder Farbanpassung

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
       colors: {
         'street-menu-bg-gray' : '#5B6569',
         'active-block-turquoise' : '#95E8FF',
-        'back-folder-gray' : '#424A4C',
+        'backfolder-gray' : '#424A4C',
         'street-menu-tile-bg-turquoise' : '#E4F9FF',
         'bulldozer-yellow' : '#FFD941',
         'bulldozer-gray' : '#4B5357',
