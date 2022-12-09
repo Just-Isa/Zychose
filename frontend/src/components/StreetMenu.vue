dc8bef1b9e4279ece33a0db325d3d794b1739794
Antonia Eckert <antonia.eckert@student.hs-rm.de>
2022-12-09 18:26:38 +0100

    [#50][ADDED]: Styling für Tabs

--- frontend/src/components/StreetMenu.vue
+++ frontend/src/components/StreetMenu.vue
@@ -5,7 +5,7 @@ Source for basic tab idea: https://vuejsexamples.com/tabbed-content-with-vue-js/
 
 <template>
   <div class="fixed bottom-6 left-1/2 -translate-x-1/2">
-    <div class="flex gap-3">
+    <div class="tabs flex gap-3">
       <!--- 
         Tabs jetzt nicht als Komponente - werden nicht weiter modifiziert/weiterbenutzt, 
         ergo einfacher einfach nur ein Tag mit Klickfunktion und CSS zu haben.
@@ -73,3 +73,18 @@ function changeTab(newTabNumber: number) {
   console.log("neues Tab:" + activetab.value);
 }
 </script>
+
+<style>
+/**
+mit Tailwind lässt sich die Hintergrund-Farbe des nicht aktiven Tabs leider nicht ändern
+(wie es mit tailwind funktionieren sollte, ist aber noch drin)
+ */
+
+.tabs a {
+  background-color: #424a4c;
+}
+
+.tabs a.active {
+  background-color: #5b6569;
+}
+</style>
