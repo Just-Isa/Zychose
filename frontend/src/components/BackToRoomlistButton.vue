d5bf7ec5730ada334470e43bb610b475e9ed5544
Mike Daudrich <mx.daudrich@gmail.com>
2022-12-10 12:44:25 +0100

    [#49][ADDITION]: Raum verlassen Button hinzugefügt

---
+++ frontend/src/components/BackToRoomlistButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <a class="exitToRoomList" v-on:click="hoden()" :href="'#'"> Klickung </a>
+  </main>
+</template>
+
+<script setup lang="ts">
+function hoden() {
+  location.href = "/rooms";
+  console.log("hoden");
+}
+</script>
+
+<style>
+.exitToRoomList {
+  position: fixed;
+  z-index: 999;
+}
+</style>
--- frontend/src/views/EditorView.vue
+++ frontend/src/views/EditorView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import MultiplayerCursors from "../components/MultiplayerCursors.vue";
 import GridComponent from "@/components/grid/GridComponent.vue";
+import BackToRoomlistButtonVue from "@/components/BackToRoomlistButton.vue";
 </script>
 
 <template>
   <main>
+    <BackToRoomlistButtonVue />
     <MultiplayerCursors />
     <GridComponent v-bind:gridSize="100"></GridComponent>
   </main>
