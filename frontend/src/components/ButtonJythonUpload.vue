257fe1a513801a4802c0d439c4fd0b4d32b50d77
Antonia Eckert <antonia.eckert@student.hs-rm.de>
2022-12-12 13:47:22 +0100

    [#119][ADDED]: Upload-Button Komponente

---
+++ frontend/src/components/ButtonJythonUpload.vue
@@ -0,0 +1,8 @@
+<!-- https://malcoded.com/posts/vue-file-upload-ts/ -->
+<template>
+  <div class="jython-upload"></div>
+  <div>Add file</div>
+  <input type="file" />
+</template>
+
+<script setup lang="ts"></script>
