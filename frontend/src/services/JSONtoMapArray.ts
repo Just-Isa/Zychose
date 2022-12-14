6a78c14f16a54958614e92694614d665f7650206
Mike Daudrich <mx.daudrich@gmail.com>
2022-12-14 12:16:47 +0100

    [#140][ADDITION]: json parser umgebaut

--- frontend/src/services/JSONtoMapArray.ts
+++ frontend/src/services/JSONtoMapArray.ts
@@ -4,21 +4,9 @@
  * @param jsondata
  * @returns streetArray
  */
-export function generateMapArray(jsondata: object) {
+export function generateMapArray(jsondata: object[]) {
   const size = 100;
-  const rotations = new Array();
-  const streetTypes = new Array();
   const streetArray: string[][] = new Array();
-
-  // dynamically read street types
-  for (const element of Object.keys(jsondata)) {
-    streetTypes.push(element);
-  }
-
-  // dynamically read street type orientations/rotations (in this case: from the first street type)
-  for (const element of Object.keys(Object(jsondata)[streetTypes[0]])) {
-    rotations.push(element);
-  }
 
   // initialize empty street array
   for (let i = 0; i < size; i++) {
@@ -29,16 +17,10 @@ export function generateMapArray(jsondata: object) {
   }
 
   // fill street array with types according to given coordinates
-  for (const i in streetTypes) {
-    const streetType = Object(jsondata)[streetTypes[i]];
-    for (const j in rotations) {
-      const rotation = Object(streetType)[rotations[j]];
-      for (const z in rotation) {
-        const coords = rotation[z];
-        streetArray[coords[0]][coords[1]] = streetTypes[i] + ":" + rotations[j];
-      }
-    }
-  }
+  jsondata.forEach(function (obj) {
+    streetArray[Object(obj)["posX"]][Object(obj)["posY"]] =
+      Object(obj)["streetType"] + ":" + Object(obj)["rotation"];
+  });
 
   return streetArray;
 }
