80108e24552868c79d1abdcb6e05d70c10eae32f
Isa Kilic <Is4Kilic@hotmail.com>
2022-12-14 23:38:51 +0100

    [#130][REFACTOR]: Helper class deleted

--- frontend/src/views/ResetInfoView.vue
+++ frontend/src/views/ResetInfoView.vue
@@ -5,7 +5,6 @@
 
 <script setup lang="ts">
 import { onMounted } from "vue";
-import { resetEverything } from "@/helpers/ResetHelper";
 import { deleteSessionId } from "@/helpers/SessionIDHelper";
 
 onMounted(() => {
@@ -16,4 +15,27 @@ onMounted(() => {
     location.href = "/rooms";
   }, 100);
 });
+
+function resetEverything() {
+  const DEST = "/api/reset";
+  fetch(DEST, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        console.log("Error @ Restarting everything.");
+      } else {
+        return response.text();
+      }
+    })
+    .then(() => {
+      console.log("Done! Game has been reset. Please refresh.");
+    })
+    .catch(() => {
+      console.log("Error @ Restarting everything.");
+    });
+}
 </script>
