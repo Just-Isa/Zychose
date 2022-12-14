97bfa4775d5ef21bcf38de488ce574e0a0411391
Linda Conradi <linda.conradi@student.hs-rm.de>
2022-12-14 10:51:17 +0100

    [#128][ADD]: Performance Test für die Parsings geschrieben.

--- frontend/src/services/JSONparser.ts
+++ frontend/src/services/JSONparser.ts
@@ -2,13 +2,21 @@ import type { IStreetInformation } from "./useStreets";
 import { useRoom } from "./useRoom";
 import { useStreets } from "./useStreets";
 
+
+/**
+ * Function to parse the streetState into a JSON format
+ * @param {IStreetInformation[]} streets array of all streets in form of our generated streetState
+ */
 export function gridToJson(streets: IStreetInformation[]) {
   const { updateRoomMap } = useRoom();
   updateRoomMap(JSON.stringify(streets));
-
+  
   /*jsonToState wird am Ende nicht an dieser Stelle aufgerufen,
     das Linting weint aber sonst wegen unused function rum und zum testen hat es hier gereicht.
     Der Aufruf erfolgt dann nach einer Nachricht vom Backend.
+
+    WICHTIG: jsonParseTime.test.js wurde ohne diesen Funktionsaufruf ausgeführt, da er nicht an dieser Stelle bleibt.
+    Zum testen also auskommentieren oder ähnliches.
   */
   jsonToState(JSON.stringify(streets));
 }
@@ -18,7 +26,7 @@ export function gridToJson(streets: IStreetInformation[]) {
  * This function is necessary to be able to synchronize the RoomMap for all users in a room.
  * @param {string} roomMapAsString - stringified JSON-Object of our streets
  */
-function jsonToState(roomMapAsString: string) {
+export function jsonToState(roomMapAsString: string) {
   const { recieveNewStreetState } = useStreets();
   const newState = JSON.parse(roomMapAsString);
   recieveNewStreetState(newState);
---
+++ frontend/test/jsonParseTime.test.js
@@ -0,0 +1,34 @@
+import { expect, test } from 'vitest'
+import {gridToJson, jsonToState} from '../src/services/JSONparser.ts'
+
+
+let streetState = [];
+    for (let row = 1; row<= 100; row++) {
+        for (let col = 1; col <= 100; col++) {
+            const newStreet = {
+                streetType: "straight-road",
+                rotation: 90,
+                posX: col,
+                posY: row
+            }
+            streetState.push(newStreet);
+        }
+    }
+
+test('gridstate parsing to json time elapsed', () => {
+    const start = new Date().getTime();
+    gridToJson(streetState);
+    const elapsed = new Date().getTime() - start;
+    const elapsedInSeconds = elapsed/1000;
+    console.log(console.log(`${elapsedInSeconds}s`));
+    expect(elapsedInSeconds).toBeLessThan(0.01);
+})
+
+test('parsing from json to state time elapsed', () => {
+    const start = new Date().getTime();
+    jsonToState(JSON.stringify(streetState));
+    const elapsed = new Date().getTime() - start;
+    const elapsedInSeconds = elapsed/1000;
+    console.log(console.log(`${elapsedInSeconds}s`));
+    expect(elapsedInSeconds).toBeLessThan(0.01);
+})
