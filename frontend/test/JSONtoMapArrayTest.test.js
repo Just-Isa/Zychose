e21d33776aa0354300b2a45018e32f70d2d55c5e
Mike Daudrich <mx.daudrich@gmail.com>
2022-12-14 12:18:33 +0100

    [#140][UPDATE]: linting

--- frontend/test/JSONtoMapArrayTest.test.js
+++ frontend/test/JSONtoMapArrayTest.test.js
@@ -1,16 +1,16 @@
 import { generateMapArray } from "../src/services/JSONtoMapArray.ts";
 
 test("generateMapArray", () => {
-  let text = 
+  let text =
     '[{"streetType":"road_straight",' +
     '"rotation":90,' +
     '"posX":1,' +
     '"posY":1' +
-    '},' +
+    "}," +
     '{"streetType":"road_straight",' +
     '"rotation":180,' +
     '"posX":1,' +
-    '"posY":3}]'
+    '"posY":3}]';
 
   const data = JSON.parse(text);
   const streetArray = generateMapArray(data);
