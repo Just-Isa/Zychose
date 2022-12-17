efa7d72f11534e0476e81f107a866c20892c7386
Antonia Eckert <antonia.eckert@student.hs-rm.de>
2022-12-17 12:35:56 +0100

    [#67][ADDED]: StreetMenuTab in eigene Komponente ausgelagert

--- frontend/src/components/StreetMenu.vue
+++ frontend/src/components/StreetMenu.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="fixed bottom-6 left-1/2 -translate-x-1/2 pointer-events-none">
-    <div class="tabs flex gap-3">
+    <!-- <div class="tabs flex gap-3">
       <a
         @click="changeTab(1)"
         v-bind:class="[
@@ -8,7 +8,7 @@
         ]"
         class="pointer-events-auto bg-street-menu-bg-gray px-4 pt-1 rounded-tl-lg rounded-tr-lg hover:cursor-pointer"
       >
-        <img src="../assets/img/car-pictogram.svg" />
+        <img src="../assets/img/car-pictogram.svg" alt="car-menu" />
       </a>
       <a
         @click="changeTab(2)"
@@ -17,20 +17,35 @@
         ]"
         class="pointer-events-auto bg-street-menu-bg-gray px-4 pt-1 rounded-tl-lg rounded-tr-lg hover:cursor-pointer"
       >
-        <img src="../assets/img/bicycle-pictogram.svg"
+        <img src="../assets/img/bicycle-pictogram.svg" alt="bicycle-menu"
       /></a>
+    </div> -->
+    <div v-for="vehicle in vehicleTypes" v-bind:key="vehicle[1]">
+      <StreetMenuTab
+        :vehicleType="vehicle[0]"
+        :imgSrc="vehicle[1]"
+        @click="changeTab(vehicle[0])"
+        v-bind:class="[
+          activetab === vehicle[0]
+            ? 'active shadow-lg'
+            : 'bg-inactive-folder-gray',
+        ]"
+      ></StreetMenuTab>
     </div>
     <div class="flex justify-items-center items-center">
-      <StreetMenuFolder
-        v-if="activetab === 1"
-        class="pointer-events-auto tabcontent flex rounded-b-lg rounded-tr-lg"
-        :types="streetTypes"
-      />
-      <StreetMenuFolder
-        v-if="activetab === 2"
-        class="pointer-events-auto tabcontent flex rounded-b-lg rounded-tr-lg"
-        :types="bikeTypes"
-      />
+      <div id="streetMenuFolder">
+        <StreetMenuFolder
+          v-if="activetab === 'streetTypes'"
+          class="pointer-events-auto tabcontent flex rounded-b-lg rounded-tr-lg"
+          :types="streetTypes"
+        />
+        <StreetMenuFolder
+          v-if="activetab === 'bikeTypes'"
+          class="pointer-events-auto tabcontent flex rounded-b-lg rounded-tr-lg"
+          :types="bikeTypes"
+        />
+      </div>
+
       <BullDozerBtn
         cursorSrc="src/assets/img/bulldozer-cursor.svg"
         class="pointer-events-auto"
@@ -43,29 +58,38 @@
 import { ref } from "vue";
 import StreetMenuFolder from "./StreetMenuFolder.vue";
 import BullDozerBtn from "./BullDozerBtn.vue";
+import StreetMenuTab from "./StreetMenuTab.vue";
 import { useTile } from "@/services/useTileState";
 
 const { changeCurrentTileType, toggleBulldozer } = useTile();
 
 /**
- * activetab: numeration of tabs from left to right
+ * activetab: currently string representing the corresponding vehicle
  */
-const activetab = ref(1);
+const activetab = ref("streetTypes");
 
 const streetTypes = ["straight", "tCrossing", "curve", "crossing"];
 const bikeTypes = ["straight", "tCrossing"];
 
 /**
+ * TODO: wie bekommt man die Daten? auch über json, wie die StreetBlocks?
+ */
+const vehicleTypes = [
+  ["streetTypes", "car-pictogram.svg"],
+  ["bikeTypes", "src/assets/img/bicycle-pictogram.svg"],
+];
+
+/**
  * function to change the selected tab and switch the displayed content for the tile menu
  * @param newTabNumber number of the newly selected tab
  */
-function changeTab(newTabNumber: number) {
+function changeTab(vehicleType: string) {
   changeCurrentTileType("");
   toggleBulldozer(false);
   // set active StreetTile to "" when switching between folders
   console.log("altes Tab: " + activetab.value);
 
-  activetab.value = newTabNumber;
+  activetab.value = vehicleType;
 
   console.log("neues Tab:" + activetab.value);
 }
---
+++ frontend/src/components/StreetMenuTab.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex">
+    <a
+      class="mr-2 pointer-events-auto bg-street-menu-bg-gray px-4 pt-1 rounded-tl-lg rounded-tr-lg hover:cursor-pointer"
+    >
+      <img src="src/assets/img/${imgSrc}" alt="props.vehicleType" />
+    </a>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  vehicleType: string;
+  imgSrc: string;
+}>();
+
+console.log(props.imgSrc);
+</script>
