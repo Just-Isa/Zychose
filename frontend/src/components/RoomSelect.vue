fc06235fd826fa1cfdbcd22f7238e4eca349d3e3
Chantal Brauer <chantal.brauer@student.hs-rm.de>
2022-12-19 14:10:01 +0100

    [#100][REFACTOR] Styling Rooms Übersicht

--- frontend/src/components/RoomSelect.vue
+++ frontend/src/components/RoomSelect.vue
@@ -1,16 +1,22 @@
 <template>
-  <h1 style="font-size: 20px; text-align: center">ROOMS</h1>
-  <br />
-  <div v-for="room in roomListItems" v-bind:key="room.roomNumber">
-    <a style="color: red" :href="'/' + room.roomNumber"
-      >Raum {{ room.roomNumber }}: {{ room.roomName }}<br
-    /></a>
-    <div
-      v-for="user in room.userList"
-      v-bind:key="user.sessionID"
-      style="margin-left: 20px"
-    >
-      <p>{{ user.userName }}</p>
+  <div class="grid grid-cols-3 my-8 mx-20">
+    <div class="w-1/12 self-end">
+      <GoBackButtonVue />
+    </div>
+    <span class="font-bold text-6xl text-center self-center">SERVERS</span>
+    <div class="w-1/12 place-self-end pr-2.5">
+      <AddButtonVue />
+    </div>
+  </div>
+
+  <div v-for="room in roomListItems" v-bind:key="room.roomNumber"
+    class="grid grid-cols-2 gp-96 py-5 pl-5 my-5 mx-20 bg-gray-500 drop-shadow-md rounded-md">
+    <span class="font-bold text-xl">Raum {{ room.roomNumber }}</span>
+    <div class="flex grid-cols-3 justify-end pr-5">
+      <span class="text-right text-lg">{{ room.userList.length }} Players</span>
+      <button class="bg-teal-500 font-semibold text-lg rounded-md px-2 mx-2.5">Upload Script</button>
+      <button v-on:click="$router.push('/' + room.roomNumber)"
+        class="bg-green-500 font-semibold text-lg rounded-md px-2 w-20">Join</button>
     </div>
   </div>
 </template>
@@ -20,6 +26,8 @@
 import { useRoomBox } from "@/services/useRoomList";
 import { onMounted, computed } from "vue";
 import { useUser } from "@/services/useUser";
+import AddButtonVue from "./AddButton.vue";
+import GoBackButtonVue from "./GoBackButton.vue";
 
 const { roomListState, getRoomList } = useRoomBox();
 const { createUser } = useUser();
@@ -36,3 +44,11 @@ onMounted(() => {
   }, 600);
 });
 </script>
+
+<style>
+body {
+  font-family: Poppins;
+  color: white;
+  background-color: rgb(63 63 70);
+}
+</style>
