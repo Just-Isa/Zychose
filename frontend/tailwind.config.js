2c9a54c7387cf949662c110db106bd97ddc6f88d
Chantal Brauer <chantal.brauer@student.hs-rm.de>
2022-12-20 04:01:10 +0100

    [#100][ADDITION] Buttons, Farben in Tw Config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.vue"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'Poppins': ['Poppins', 'sans']
+    },
+    extend: {
+      colors: {
+        'blue-custom': '#00C8FF',
+        'green-custom': '#34D95A',
+        'background': '#424A4C',
+        'light-gray-custom': '#6E787B',
+      }
+    },
+
   },
   plugins: [],
 };
