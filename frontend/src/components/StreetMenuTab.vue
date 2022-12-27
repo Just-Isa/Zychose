5dc200b4bad55c07fa8121429dd63791bce5fae9
Just-Isa <Is4Kilic@hotmail.com>
2022-12-27 10:14:13 +0100

    [#50][REFACTOR]: Classes pulled from div into Button in StreetMenuTab

--- frontend/src/components/StreetMenuTab.vue
+++ frontend/src/components/StreetMenuTab.vue
@@ -1,9 +1,11 @@
 <template>
-  <div
-    class="p-1 -mb-[4px] max-h-7 pointer-events-auto bg-street-menu-bg-gray px-4 pt-1 rounded-tl-lg rounded-tr-lg hover:cursor-pointer"
-  >
+  <div>
     <button>
-      <img :src="`src/assets/img/${props.imgSrc}`" :alt="props.vehicleType + ' icon'" />
+      <img
+        :src="`src/assets/img/${props.imgSrc}`"
+        :alt="props.vehicleType + ' icon'"
+        class="p-1 -mb-[4px] max-h-7 pointer-events-auto bg-street-menu-bg-gray px-4 pt-1 rounded-tl-lg rounded-tr-lg hover:cursor-pointer"
+      />
     </button>
   </div>
 </template>
