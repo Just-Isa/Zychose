92028003a18f937af289a0d85c1edaad8c249773
Chantal Brauer <chantal.brauer@student.hs-rm.de>
2022-12-27 16:28:30 +0100

    [#100][REFACTOR] CR Styling anpassung

--- frontend/src/views/RoomSelectView.vue
+++ frontend/src/views/RoomSelectView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-background fixed w-full h-full">
+  <div class="bg-back-folder-gray fixed w-full h-full">
   <div class="flex justify-between my-8 mx-20">
     <GoBackButton class="drop-shadow-md place-self-end" v-on:click="$router.push('/')" />
     <h1 class="font-bold text-6xl text-white font-sans self-center uppercase">Servers</h1>
@@ -7,13 +7,13 @@
   </div>
 
   <div v-for="room in roomListItems" v-bind:key="room.roomNumber"
-    class="flex justify-between py-5 pl-5 my-5 mx-20 bg-light-gray-custom text-white font-sans drop-shadow-md rounded-md">
+    class="flex justify-between items-center py-3 pl-5 my-5 mx-20 bg-room-list-bg-gray text-white font-sans drop-shadow-md rounded-md">
     <span class="font-bold text-xl">Raum {{ room.roomNumber }}</span>
-    <div class="flex justify-end pr-5 text-lg">
+    <div class="flex justify-end items-center pr-3 text-lg">
       <span>{{ room.userList.length }} Players</span>
-      <button class="bg-blue-custom font-semibold rounded px-2 mx-2.5">Upload Script</button>
+      <button class="bg-button-blue-bright font-semibold rounded w-40 py-1 ml-3 mr-4">Upload Script</button>
       <button v-on:click="$router.push('/' + room.roomNumber)"
-        class="bg-green-custom font-semibold rounded px-2 w-20">Join</button>
+        class="bg-button-green font-semibold rounded w-16 py-1">Join</button>
     </div>
   </div>
 </div>
