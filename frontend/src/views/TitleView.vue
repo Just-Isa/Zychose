7ddd19a99a299a01f8e3729ba8d489b92fbfdd04
Lara Heibel <lara.heibel@student.hs-rm.de>
2023-01-03 23:14:21 +0100

    [#202][REFACTOR]: Anpassung Pfad analog zu Issue/#198 + View-Name

--- frontend/src/views/TitleView.vue
+++ frontend/src/views/TitleView.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="bg-back-folder-gray w-screen h-screen text-white pt-20 flex justify-center items-center bg-[url('/src/assets/img/fallback-title-bg.jpg')] bg-center bg-cover"
+    class="bg-back-folder-gray w-screen h-screen text-white pt-20 flex justify-center items-center bg-[url('@/assets/img/fallback-title-bg.jpg')] bg-center bg-cover"
   >
     <div class="text-center">
       <h1 class="font-headline text-[15rem] leading-[15rem] uppercase">
