3e9b048dbe92c5e98cccbd5fa48d106fbc8ed10e
Ferdinand Mentzen <ferdinand.mentzen@student.hs-rm.de>
2023-01-11 16:33:19 +0100

    [#131][REFACTOR]: Linting

--- frontend/src/helpers/ConsoleLoggingManager.ts
+++ frontend/src/helpers/ConsoleLoggingManager.ts
@@ -1,21 +1,19 @@
 import jsonconfig from "../../../swtp.config.json";
 
 interface NullConsole {
-    log: Function;
-    error: Function;
+  log: Function;
+  error: Function;
 }
 
 let logger: Console | NullConsole = {} as Console;
 
-if(jsonconfig.showConsoleOutput) {
-    logger = window.console;
+if (jsonconfig.showConsoleOutput) {
+  logger = window.console;
 } else {
-    logger = {
-        log: (...args: any) => {},
-        error: (...args: any) => {},
-    };
+  logger = {
+    log: (...args: any) => {},
+    error: (...args: any) => {},
+  };
 }
 
-export {
-    logger
-}
+export { logger };
