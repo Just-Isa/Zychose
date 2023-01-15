97d5db6fac3240c1a2472530e1f08117c8db3ede
Chantal Brauer <chantal.brauer@student.hs-rm.de>
2023-01-15 18:14:46 +0100

    [#96][REFACTOR] auslagern Kartenlogik aus Room

---
+++ src/main/java/de/hsrm/mi/team3/swtp/domain/Roadmap.java
@@ -0,0 +1,71 @@
+package de.hsrm.mi.team3.swtp.domain;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public class Roadmap {
+
+    private Tile[][] tileMap;
+
+    public Roadmap(String mapstring) {
+        // split String and convert into Tiles
+    }
+
+    public Roadmap(Roadmap existingMap) {
+        // copy maps
+    }
+
+    public Map<VehicleNeighbour, Tile> getNeighbours(int x, int y, int rotation) {
+        Map<VehicleNeighbour, Tile> neighbours = new EnumMap<>(VehicleNeighbour.class);
+        if (rotation == 90) {
+            // Drehung nach Bildschirm-rechts
+            neighbours.put(VehicleNeighbour.VEHICLELEFT, getTile(x, y - 1));
+            neighbours.put(VehicleNeighbour.VEHICLETOPLEFT, getTile(x + 1, y - 1));
+            neighbours.put(VehicleNeighbour.VEHICLETOP, getTile(x + 1, y));
+            neighbours.put(VehicleNeighbour.VEHICLETOPRIGHT, getTile(x + 1, y + 1));
+            neighbours.put(VehicleNeighbour.VEHICLERIGHT, getTile(x, y + 1));
+        } else if (rotation == 180) {
+            // Drehung nach Bildschirm-unten
+            neighbours.put(VehicleNeighbour.VEHICLELEFT, getTile(x + 1, y));
+            neighbours.put(VehicleNeighbour.VEHICLETOPLEFT, getTile(x + 1, y + 1));
+            neighbours.put(VehicleNeighbour.VEHICLETOP, getTile(x, y + 1));
+            neighbours.put(VehicleNeighbour.VEHICLETOPRIGHT, getTile(x - 1, y + 1));
+            neighbours.put(VehicleNeighbour.VEHICLERIGHT, getTile(x - 1, y));
+
+        } else if (rotation == 270) {
+            // Drehung nach Bildschirm-links
+            neighbours.put(VehicleNeighbour.VEHICLELEFT, getTile(x, y + 1));
+            neighbours.put(VehicleNeighbour.VEHICLETOPLEFT, getTile(x - 1, y + 1));
+            neighbours.put(VehicleNeighbour.VEHICLETOP, getTile(x - 1, y));
+            neighbours.put(VehicleNeighbour.VEHICLETOPRIGHT, getTile(x - 1, y - 1));
+            neighbours.put(VehicleNeighbour.VEHICLERIGHT, getTile(x, y - 1));
+
+        } else {
+            // keine Drehung Ausrichtung nach Bildschirm-oben
+            neighbours.put(VehicleNeighbour.VEHICLELEFT, getTile(x - 1, y));
+            neighbours.put(VehicleNeighbour.VEHICLETOPLEFT, getTile(x - 1, y - 1));
+            neighbours.put(VehicleNeighbour.VEHICLETOP, getTile(x, y - 1));
+            neighbours.put(VehicleNeighbour.VEHICLETOPRIGHT, getTile(x + 1, y - 1));
+            neighbours.put(VehicleNeighbour.VEHICLERIGHT, getTile(x + 1, y));
+        }
+
+        return neighbours;
+    }
+
+    public void setTile(String tilename, int rotation, int x, int y, boolean blocked) {
+        this.tileMap[x][y] = new Tile(tilename, rotation, x, y, blocked);
+    }
+
+    public void setTile(Tile existingTile) {
+        this.tileMap[existingTile.getTilePosition()[0]][existingTile.getTilePosition()[1]] = existingTile;
+    }
+
+    public Tile getTile(int x, int y) {
+        if (this.tileMap[x][y] != null) {
+            return this.tileMap[x][y];
+        } else {
+            return null;
+        }
+    }
+
+}
--- src/main/java/de/hsrm/mi/team3/swtp/domain/Room.java
+++ src/main/java/de/hsrm/mi/team3/swtp/domain/Room.java
@@ -1,9 +1,7 @@
 package de.hsrm.mi.team3.swtp.domain;
 
 import java.util.ArrayList;
-import java.util.EnumMap;
 import java.util.List;
-import java.util.Map;
 
 /*
  * Room class that is used within the RoomBox.
@@ -15,8 +13,7 @@ public class Room {
 	private int roomNumber;
 	private List<User> userList;
 	private String jythonScript;
-	private String roomMap = "streetType: straight-road,rotation:90,posX:1,posY:1},{streetType:straight-road,rotation:90,posX:1,posY:2}";
-	private Tile[][] tileMap;
+	private Roadmap roadMap;
 
 	public Room(int roomNumber) {
 		this.roomName = "default-name";
@@ -30,10 +27,6 @@ public class Room {
 		this.roomNumber = roomNumber;
 		this.userList = new ArrayList<User>();
 		this.jythonScript = "";
-	}
-
-	private void analyseMap() {
-		// verwandelt String in Tiles und speichert sie in tileMap
 	}
 
 	public String getRoomName() {
@@ -73,49 +66,15 @@ public class Room {
 	}
 
 	public void setRoomMap(String map) {
-		this.roomMap = map;
+		this.roadMap = new Roadmap(map);
 	}
 
-	public String getRoomMap() {
-		return this.roomMap;
+	public void setRoomMap(Roadmap existingMap) {
+		this.roadMap = new Roadmap(existingMap);
 	}
 
-	public Map<VehicleNeighbour, Tile> getNeighbours(int currentX, int currentY, int rotation) {
-		Map<VehicleNeighbour, Tile> neighbours = new EnumMap<>(VehicleNeighbour.class);
-		int x = currentX, y = currentY;
-		analyseMap();
-		if (rotation == 90) {
-			// assumed einfache Drehung nach Bildschirm-rechts
-			neighbours.put(VehicleNeighbour.VEHICLELEFT, tileMap[x][y - 1]);
-			neighbours.put(VehicleNeighbour.VEHICLETOPLEFT, tileMap[x + 1][y - 1]);
-			neighbours.put(VehicleNeighbour.VEHICLETOP, tileMap[x + 1][y]);
-			neighbours.put(VehicleNeighbour.VEHICLETOPRIGHT, tileMap[x + 1][y + 1]);
-			neighbours.put(VehicleNeighbour.VEHICLERIGHT, tileMap[x][y + 1]);
-		} else if (rotation == 180) {
-			// assumed Drehung nach Bildschirm-unten
-			neighbours.put(VehicleNeighbour.VEHICLELEFT, tileMap[x + 1][y]);
-			neighbours.put(VehicleNeighbour.VEHICLETOPLEFT, tileMap[x + 1][y + 1]);
-			neighbours.put(VehicleNeighbour.VEHICLETOP, tileMap[x][y + 1]);
-			neighbours.put(VehicleNeighbour.VEHICLETOPRIGHT, tileMap[x - 1][y + 1]);
-			neighbours.put(VehicleNeighbour.VEHICLERIGHT, tileMap[x - 1][y]);
-
-		} else if (rotation == 270) {
-			// assumed Drehung nach Bildschirm-links
-			neighbours.put(VehicleNeighbour.VEHICLELEFT, tileMap[x][y + 1]);
-			neighbours.put(VehicleNeighbour.VEHICLETOPLEFT, tileMap[x - 1][y + 1]);
-			neighbours.put(VehicleNeighbour.VEHICLETOP, tileMap[x - 1][y]);
-			neighbours.put(VehicleNeighbour.VEHICLETOPRIGHT, tileMap[x - 1][y - 1]);
-			neighbours.put(VehicleNeighbour.VEHICLERIGHT, tileMap[x][y - 1]);
-
-		} else {
-			// keine Drehung Ausrichtung nach Bildschirm-oben
-			neighbours.put(VehicleNeighbour.VEHICLELEFT, tileMap[x - 1][y]);
-			neighbours.put(VehicleNeighbour.VEHICLETOPLEFT, tileMap[x - 1][y - 1]);
-			neighbours.put(VehicleNeighbour.VEHICLETOP, tileMap[x][y - 1]);
-			neighbours.put(VehicleNeighbour.VEHICLETOPRIGHT, tileMap[x + 1][y - 1]);
-			neighbours.put(VehicleNeighbour.VEHICLERIGHT, tileMap[x + 1][y]);
-		}
-
-		return neighbours;
+	public Roadmap getRoomMap() {
+		return this.roadMap;
 	}
+
 }
