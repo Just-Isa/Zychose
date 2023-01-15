2ad3bd6800d8f571f41d44b8f479aa36323e1d1e
Chantal Brauer <chantal.brauer@student.hs-rm.de>
2023-01-15 12:29:45 +0100

    [#96][ADDITION] VehicleBot Grundstruktur

--- src/main/java/de/hsrm/mi/team3/swtp/domain/VehicleBot.java
+++ src/main/java/de/hsrm/mi/team3/swtp/domain/VehicleBot.java
@@ -1,6 +1,59 @@
 package de.hsrm.mi.team3.swtp.domain;
 
-public class VehicleBot extends Vehicle {
+public class VehicleBot {
 
-  private VehicleBehaviour behaviours;
+  private VehicleBehaviour behaviour = VehicleBehaviour.DEFENSIVE;
+  private int[] currentPos = { 0, 0 };
+  private int currentRotation = 0;
+
+  public VehicleBot(int[] positon, int rotation, VehicleBehaviour behaviour) {
+    this.currentPos = positon;
+    this.currentRotation = rotation;
+    this.behaviour = behaviour;
+  }
+
+  public VehicleBot(int[] positon, int rotation) {
+    this.currentPos = positon;
+    this.currentRotation = rotation;
+  }
+
+  public VehicleBot() {
+  }
+
+  public void moveToNextTile() {
+    // this.currentPos[0]=map.getX[this.getCurrentX()+direction]
+  }
+
+  public void turn(int rotation) {
+    this.currentRotation += rotation;
+    moveToNextTile();
+  }
+
+  public int getCurrentX() {
+    return this.currentPos[0];
+  }
+
+  public void setCurrentX(int x) {
+    this.currentPos[0] = x;
+  }
+
+  public int getCurrentY() {
+    return this.currentPos[1];
+  }
+
+  public void setCurrentY(int y) {
+    this.currentPos[1] = y;
+  }
+
+  public int getCurrentRotation() {
+    return this.currentRotation;
+  }
+
+  public void setCurrentRotation(int rotation) {
+    this.currentRotation = rotation;
+  }
+
+  public VehicleBehaviour getBehaviour() {
+    return this.behaviour;
+  }
 }
