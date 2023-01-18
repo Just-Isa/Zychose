04c1fc56bcd84ab497bf53a123cabe81fa91c0d2
Lara Heibel <lara.heibel@student.hs-rm.de>
2023-01-18 19:38:03 +0100

    [#238][ADDITION] Weiterleitung zu 2D per Button und ESC mit Fade-to-white

---
+++ frontend/src/views/GameView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import ThreeDGame from "@/views/threeD/ThreeDGame.vue";
+import Exit3DButton from "@/components/Exit3DButton.vue";
+</script>
+
+<template>
+  <main>
+    <Exit3DButton />
+    <ThreeDGame />
+  </main>
+</template>
