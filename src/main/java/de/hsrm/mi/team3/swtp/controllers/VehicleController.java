775c6c474ab2ee0eaa31f79f99f4f5069ba8cb58
Pablo Schneider <pablo.schneider@chaos1.de>
2023-01-20 18:54:15 +0100

    [#157][REFACTOR] spotless

--- src/main/java/de/hsrm/mi/team3/swtp/controllers/VehicleController.java
+++ src/main/java/de/hsrm/mi/team3/swtp/controllers/VehicleController.java
@@ -20,17 +20,13 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class VehicleController {
   Logger logger = LoggerFactory.getLogger(VehicleController.class);
-  @Autowired
-  VehicleService vehicleService;
+  @Autowired VehicleService vehicleService;
 
-  @Autowired
-  BackendInfoService bInfoService;
+  @Autowired BackendInfoService bInfoService;
 
-  @Autowired
-  RoomBoxService roomBoxService;
+  @Autowired RoomBoxService roomBoxService;
 
-  @Autowired
-  RoomService roomService;
+  @Autowired RoomService roomService;
 
   /**
    * Receives a command from client to execute vehicleservice Methods
@@ -44,12 +40,14 @@ public class VehicleController {
       @DestinationVariable int roomNumber) {
 
     List<VehicleCommands> commands = commandVehicleMessage.commands();
-    Vehicle vehicle = roomService.getUserByID(roomNumber, commandVehicleMessage.userSessionId()).getVehicle();
+    Vehicle vehicle =
+        roomService.getUserByID(roomNumber, commandVehicleMessage.userSessionId()).getVehicle();
     if (vehicle == null) {
       roomService
           .getUserByID(roomNumber, commandVehicleMessage.userSessionId())
           .setVehicle(new Vehicle("car"));
-      vehicle = roomService.getUserByID(roomNumber, commandVehicleMessage.userSessionId()).getVehicle();
+      vehicle =
+          roomService.getUserByID(roomNumber, commandVehicleMessage.userSessionId()).getVehicle();
     }
     if (!commands.contains(VehicleCommands.FORWARD)
         && !commands.contains(VehicleCommands.BACKWARD)) {
