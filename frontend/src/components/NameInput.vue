a527e4a2ba7af3126fceb5fac72ea3a6a9298047
Lara Heibel <lara.heibel@student.hs-rm.de>
2023-01-20 16:27:37 +0100

    [#220][ADDITON] Eingabefeld für Namen im Frontend

---
+++ frontend/src/components/NameInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <label for="name">Wie dürfen wir Dich nennen?</label><br />
+    <input
+      class="text-black p-2 mt-2 w-60 rounded"
+      v-model="name"
+      placeholder="Name"
+      type="text"
+      name="name"
+      id="name"
+    />
+    <br />
+    <button
+      type="submit"
+      class="bg-button-green font-semibold rounded py-4 px-8 text-4xl mt-10 mb-10"
+    >
+      Play
+    </button>
+  </form>
+</template>
+
+<script setup lang="ts">
+let name = "";
+function submitForm(submitEvent: any) {
+  name = submitEvent.target.elements.name.value;
+  console.log(name);
+  if (name != "") {
+    location.href = "/rooms";
+  }
+}
+</script>
--- frontend/src/views/TitleView.vue
+++ frontend/src/views/TitleView.vue
@@ -1,15 +1,14 @@
+<script setup lang="ts">
+import NameInput from "@/components/NameInput.vue";
+</script>
+
 <template>
   <div
     class="bg-back-folder-gray w-screen h-screen text-white pt-20 flex justify-center items-center bg-[url('/assets/img/fallback-title-bg.jpg')] bg-center bg-cover"
   >
     <div class="text-center">
       <h1 class="font-bold text-[10rem] uppercase">Zychose</h1>
-      <button
-        class="bg-button-green font-semibold rounded py-4 px-8 text-4xl mb-10"
-        v-on:click="$router.push('/rooms')"
-      >
-        Play
-      </button>
+      <NameInput />
     </div>
   </div>
 </template>
