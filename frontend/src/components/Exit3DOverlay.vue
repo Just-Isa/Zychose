4a741388a9ae145c5dd7686f1a8e67cf573ae679
Lara Heibel <lara.heibel@student.hs-rm.de>
2023-01-23 15:27:04 +0100

    [#10][REFACTOR] Komponenten direkt in GameView

---
+++ frontend/src/components/Exit3DOverlay.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    id="overlay"
+    :class="
+      prop.isActive ? 'absolute duration-1000 bg-white h-screen w-screen' : ''
+    "
+  ></div>
+</template>
+
+<script setup lang="ts">
+const prop = defineProps<{
+  isActive: boolean;
+}>();
+</script>
--- frontend/src/views/GameView.vue
+++ frontend/src/views/GameView.vue
@@ -1,11 +1,34 @@
-<script setup lang="ts">
-import ThreeDGame from "@/views/threeD/ThreeDGame.vue";
-import Exit3DWrapper from "@/components/Exit3DWrapper.vue";
-</script>
-
 <template>
   <main>
-    <Exit3DWrapper />
+    <Exit3DOverlay :isActive="showOverlay" />
+    <Exit3DButton @click="changeTo2DView()" />
     <ThreeDGame />
   </main>
 </template>
+
+<script setup lang="ts">
+import ThreeDGame from "@/views/threeD/ThreeDGame.vue";
+import Exit3DOverlay from "@/components/Exit3DOverlay.vue";
+import Exit3DButton from "@/components/Exit3DButton.vue";
+import { useRoom } from "@/services/useRoom";
+import { ref } from "vue";
+const { roomState } = useRoom();
+
+let showOverlay = ref(false);
+
+window.addEventListener("keyup", (event) => {
+  if (event.key === "Escape") {
+    changeTo2DView();
+  }
+});
+
+/**
+ * Changes the View to the 2D-View
+ */
+function changeTo2DView() {
+  showOverlay.value = true;
+  setTimeout(function () {
+    location.href = "/" + roomState.room.roomNumber;
+  }, 800);
+}
+</script>
