4330c6a8ed2f95b75eb853a444fdfec947e8ee0e
Ferdinand Mentzen <ferdinand.mentzen@student.hs-rm.de>
2023-01-28 17:45:49 +0100

    [#252][REFACTOR]: Linting

--- frontend/src/views/threeD/ThreeDGame.vue
+++ frontend/src/views/threeD/ThreeDGame.vue
@@ -1,5 +1,5 @@
 <template>
-  <audio src="/assets/audio/background0.mp3" autoplay loop/>
+  <audio src="/assets/audio/background0.mp3" autoplay loop />
   <Renderer
     ref="renderer"
     resize="window"
