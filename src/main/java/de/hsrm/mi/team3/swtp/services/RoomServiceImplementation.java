1272f32b645b79d2ed828581e9d8677a5544d86b
Ferdinand Mentzen <ferdinand.mentzen@student.hs-rm.de>
2023-01-28 15:44:13 +0100

    [#253][REFACTOR]: Spotless apply

--- src/main/java/de/hsrm/mi/team3/swtp/services/RoomServiceImplementation.java
+++ src/main/java/de/hsrm/mi/team3/swtp/services/RoomServiceImplementation.java
@@ -16,12 +16,10 @@ public class RoomServiceImplementation implements RoomService {
 
   Logger logger = LoggerFactory.getLogger(RoomServiceImplementation.class);
 
-  @Autowired
-  RoomBoxServiceImplementation roomBoxService;
+  @Autowired RoomBoxServiceImplementation roomBoxService;
 
   /**
-   * This method adds a new user to a room, and changed the users
-   * currentRoomNumber respectively.
+   * This method adds a new user to a room, and changed the users currentRoomNumber respectively.
    *
    * @param room
    * @param user
@@ -78,12 +76,12 @@ public class RoomServiceImplementation implements RoomService {
   /**
    * Updates Room with new Variables
    *
-   * @param room         Room that is to be updated
+   * @param room Room that is to be updated
    * @param jythonScript new jythonScript for room
-   * @param roomMap      new roomMap for room
-   * @param roomName     new roomName for room
-   * @param roomNumber   new roomNumber for room
-   * @param userList     new userList for room
+   * @param roomMap new roomMap for room
+   * @param roomName new roomName for room
+   * @param roomNumber new roomNumber for room
+   * @param userList new userList for room
    */
   public void updateRoom(
       Room room,
@@ -101,9 +99,9 @@ public class RoomServiceImplementation implements RoomService {
 
   /**
    * Method that deletes the vehicle from a specific user
-   * 
+   *
    * @param roomNumber The roomnumber from the specific room
-   * @param sessionID  The sessionID from the specific user
+   * @param sessionID The sessionID from the specific user
    */
   @Override
   public void deleteVehicleFromUser(int roomNumber, String sessionID) {
