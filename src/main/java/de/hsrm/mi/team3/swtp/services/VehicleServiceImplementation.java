ac9e0fcab6a095a6720d8e97c642a0758a8f2256
Pablo Schneider <pablo.schneider@chaos1.de>
2023-01-29 12:00:25 +0100

    [#252][REFACTOR] CR umgesetzt

--- src/main/java/de/hsrm/mi/team3/swtp/services/VehicleServiceImplementation.java
+++ src/main/java/de/hsrm/mi/team3/swtp/services/VehicleServiceImplementation.java
@@ -13,8 +13,9 @@ public class VehicleServiceImplementation implements VehicleService {
 
   Logger logger = LoggerFactory.getLogger(VehicleService.class);
 
-  static final float DISTANCE = 8;
-  @Autowired RoomService roomService;
+  public static final float DRIVE_DISTANCE = 8;
+  @Autowired
+  RoomService roomService;
 
   /**
    * rotates the given vehicle to the left
@@ -95,8 +96,7 @@ public class VehicleServiceImplementation implements VehicleService {
         vehicle.setCurrentSpeed(newSpeed);
       }
     } else if (vehicle.getCurrentSpeed() < 0) {
-      double newSpeed =
-          Math.round(this.accelerate(vehicle, -Vehicle.RUN_OUT_SPEED) * 1000) / 1000.0;
+      double newSpeed = Math.round(this.accelerate(vehicle, -Vehicle.RUN_OUT_SPEED) * 1000) / 1000.0;
       if (newSpeed > -0.00001) {
         vehicle.setCurrentSpeed(0);
       } else {
@@ -112,15 +112,13 @@ public class VehicleServiceImplementation implements VehicleService {
    * @param vehicle
    */
   private void move(Vehicle vehicle, Room room) {
-    double[] moveTo = {0, 0, 0};
-    moveTo[0] =
-        (DISTANCE * vehicle.getCurrentSpeed() * Math.sin(vehicle.getRotationY()))
-            + vehicle.getPosX();
-    moveTo[2] =
-        (DISTANCE * vehicle.getCurrentSpeed() * Math.cos(vehicle.getRotationY()))
-            + vehicle.getPosZ();
+    double[] moveTo = { 0, 0, 0 };
+    moveTo[0] = (DRIVE_DISTANCE * vehicle.getCurrentSpeed() * Math.sin(vehicle.getRotationY()))
+        + vehicle.getPosX();
+    moveTo[2] = (DRIVE_DISTANCE * vehicle.getCurrentSpeed() * Math.cos(vehicle.getRotationY()))
+        + vehicle.getPosZ();
 
-    checkCollosion(vehicle, moveTo, room);
+    checkCollision(vehicle, moveTo, room);
 
     vehicle.setPosX(moveTo[0]);
     vehicle.setPosZ(moveTo[2]);
@@ -164,8 +162,8 @@ public class VehicleServiceImplementation implements VehicleService {
    * @param moveTo
    * @param room
    */
-  private void checkCollosion(Vehicle vehicle, double[] moveTo, Room room) {
-    checkPlayerVehicleCollosion(vehicle, moveTo, room);
+  private void checkCollision(Vehicle vehicle, double[] moveTo, Room room) {
+    checkPlayerVehicleCollision(vehicle, moveTo, room);
     // TODO: check other collisions
   }
 
@@ -176,22 +174,22 @@ public class VehicleServiceImplementation implements VehicleService {
    * @param moveTo
    * @param room
    */
-  private void checkPlayerVehicleCollosion(Vehicle vehicle, double[] moveTo, Room room) {
+  private void checkPlayerVehicleCollision(Vehicle vehicle, double[] moveTo, Room room) {
     List<Vehicle> vehicles = roomService.getVehicleList(room);
     for (Vehicle v : vehicles) {
       if (v != vehicle) {
-        double distance =
-            Math.sqrt(Math.pow(moveTo[0] - v.getPosX(), 2) + Math.pow(moveTo[2] - v.getPosZ(), 2));
-        if (distance < (vehicle.COLLISION_WIDTH + v.COLLISION_WIDTH)) {
+        double distanceBetweenVehicles = Math
+            .sqrt(Math.pow(moveTo[0] - v.getPosX(), 2) + Math.pow(moveTo[2] - v.getPosZ(), 2));
+        if (distanceBetweenVehicles < (vehicle.COLLISION_WIDTH + v.COLLISION_WIDTH)) {
 
-          double moveOtherX =
-              ((DISTANCE * 2) * vehicle.getCurrentSpeed() * Math.sin(vehicle.getRotationY()))
-                  + v.getPosX();
-          double moveOtherZ =
-              ((DISTANCE * 2) * vehicle.getCurrentSpeed() * Math.cos(vehicle.getRotationY()))
-                  + v.getPosZ();
-          v.setPosX(moveOtherX);
-          v.setPosZ(moveOtherZ);
+          double moveOtherVehicleToX = ((DRIVE_DISTANCE * 2) * vehicle.getCurrentSpeed()
+              * Math.sin(vehicle.getRotationY()))
+              + v.getPosX();
+          double moveOtherVehicleToZ = ((DRIVE_DISTANCE * 2) * vehicle.getCurrentSpeed()
+              * Math.cos(vehicle.getRotationY()))
+              + v.getPosZ();
+          v.setPosX(moveOtherVehicleToX);
+          v.setPosZ(moveOtherVehicleToZ);
 
           vehicle.setCurrentSpeed(0);
         }
